76f70c04c377e7f7f943c0145fca68283e1c7aee
Haris Amin <haris.amin_PA@hra.nhs.uk>
2024-04-30 15:53:56 +0100

    RSP 759: Implement Code-First Database Approach - Create IrasApplication database model - Create IRAS databse context and initialise in Program.cs

--- src/WebApi/Rsp.IrasService.WebApi/Program.cs
+++ src/WebApi/Rsp.IrasService.WebApi/Program.cs
@@ -2,12 +2,15 @@ using System.Reflection;
 using Microsoft.OpenApi.Models;
 using Rsp.IrasService.Application.Contracts;
 using Rsp.IrasService.Services;
+using Rsp.IrasService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 // Add services to the container.
 services.AddHealthChecks();
+services.AddDbContext<IrasContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
@@ -38,6 +41,18 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<IrasContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
 }
 
 app.UseHttpsRedirection();
