e3f51afd36b2c0a1fb45f00172356dc29add9237
Nikhil Bharathesh <nikhil.bharathesh_pa@hra.nhs.uk>
2024-06-24 16:21:50 +0100

    added probes controller

---
+++ src/WebApi/Rsp.IrasService.WebApi/Controllers/ProbesController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasService.Application.Authorization.Attributes;
+using Rsp.IrasService.Application.Commands;
+using Rsp.IrasService.Application.Queries;
+using Rsp.IrasService.Application.Requests;
+using Rsp.IrasService.Application.Responses;
+using Rsp.IrasService.Domain.Entities;
+
+namespace Rsp.IrasService.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProbesController: ControllerBase
+{
+    [HttpGet("liveness")]
+    public IActionResult Liveness()
+    {
+        return Ok();
+    }
+
+    [HttpGet("readiness")]
+    public IActionResult Readiness()
+    {
+        return Ok();
+    }
+
+}
