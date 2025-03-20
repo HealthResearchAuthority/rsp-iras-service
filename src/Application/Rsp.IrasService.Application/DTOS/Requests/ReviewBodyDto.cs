d9c02f128ab5d6dff62850155179069f6bcd01a5
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-20 09:22:00 +0000

    Updated to use BaseDto

--- src/Application/Rsp.IrasService.Application/DTOS/Requests/ReviewBodyDto.cs
+++ src/Application/Rsp.IrasService.Application/DTOS/Requests/ReviewBodyDto.cs
@@ -1,15 +1,10 @@
 ﻿namespace Rsp.IrasService.Application.DTOS.Requests;
 
-public class ReviewBodyDto
+public class ReviewBodyDto :BaseDto
 {
-    public Guid Id { get; set; }
     public string OrganisationName { get; set; } = null!;
     public string EmailAddress { get; set; } = null!;
     public List<string> Countries { get; set; } = [];
     public string? Description { get; set; }
-    public bool IsActive { get; set; } = true;
-    public DateTime CreatedDate { get; set; }
-    public DateTime UpdatedDate { get; set; }
-    public string CreatedBy { get; set; } = null!;
-    public string? UpdatedBy { get; set; }
+
 }
