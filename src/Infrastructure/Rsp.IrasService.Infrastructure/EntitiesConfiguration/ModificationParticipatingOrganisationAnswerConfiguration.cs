dbd75c99bdca05cc36075e743ea72cd6e20b7048
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-07-11 11:16:15 +0100

    - switch branch

---
+++ src/Infrastructure/Rsp.IrasService.Infrastructure/EntitiesConfiguration/ModificationParticipatingOrganisationAnswerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rsp.IrasService.Domain.Entities;
+
+namespace Rsp.IrasService.Infrastructure.EntitiesConfiguration;
+
+public class ModificationParticipatingOrganisationAnswerConfiguration : IEntityTypeConfiguration<ModificationParticipatingOrganisationAnswer>
+{
+    public void Configure(EntityTypeBuilder<ModificationParticipatingOrganisationAnswer> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder
+            .HasOne(ra => ra.ModificationParticipatingOrganisation)
+            .WithMany()
+            .HasForeignKey(r => r.ModificationParticipatingOrganisationId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
