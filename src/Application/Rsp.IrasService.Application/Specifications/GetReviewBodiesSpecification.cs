743d27493c4d79b6296636e3c7aca9bad40facd1
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-07-11 15:55:53 +0100

    RSP-3547 Implement advanced filters on the Manage review bodies page

--- src/Application/Rsp.IrasService.Application/Specifications/GetReviewBodiesSpecification.cs
+++ src/Application/Rsp.IrasService.Application/Specifications/GetReviewBodiesSpecification.cs
@@ -1,28 +1,49 @@
 ﻿using Ardalis.Specification;
+using Rsp.IrasService.Application.DTOS.Requests;
 using Rsp.IrasService.Domain.Entities;
 
 namespace Rsp.IrasService.Application.Specifications;
 
 public class GetReviewBodiesSpecification : Specification<RegulatoryBody>
 {
-    public GetReviewBodiesSpecification(int pageNumber, int pageSize, string? searchQuery)
+    public GetReviewBodiesSpecification(int pageNumber, int pageSize, ReviewBodySearchRequest? searchQuery)
     {
         var builder = Query
             .AsNoTracking()
             .AsSplitQuery();
 
-        if (!string.IsNullOrEmpty(searchQuery))
+        if (searchQuery != null)
         {
-            var splitQuery = searchQuery.Split(' ');
+            if (!string.IsNullOrEmpty(searchQuery.SearchQuery))
+            {
+                var splitQuery = searchQuery.SearchQuery.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            builder = builder.Where(x =>
-                        splitQuery.Any(word =>
-                            x.RegulatoryBodyName.Contains(word)
-                            ));
+                builder = builder.Where(x =>
+                    splitQuery.Any(word =>
+                        x.RegulatoryBodyName.Contains(word)
+                        ));
+            }
+
+            if (searchQuery.Country is { Count: > 0 })
+            {
+                var lowerCountries = searchQuery.Country
+                    .Where(c => !string.IsNullOrWhiteSpace(c))
+                    .Select(c => c.ToLower())
+                    .ToList();
+
+                builder = builder.Where(rb =>
+                    rb.Countries.Any(c =>
+                        lowerCountries.Contains(c.ToLower())));
+            }
+
+            if (searchQuery.Status != null)
+            {
+                builder.Where(x => x.IsActive == searchQuery.Status.Value);
+            }
         }
 
         builder.OrderBy(x => x.RegulatoryBodyName)
-        .Skip((pageNumber - 1) * pageSize)
-        .Take(pageSize);
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
     }
 }
