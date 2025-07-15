4e0aa5b43fb1d0227e68211a9bd743500217cdc9
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-07-15 14:07:33 +0100

    - repondent service unit tests update

---
+++ tests/UnitTests/Rsp.IrasService.UnitTests/Services/RespondentServiceTests/SaveModificationParticipatingOrganisationAnswers.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rsp.IrasService.Application.Contracts.Repositories;
+using Rsp.IrasService.Application.DTOS.Requests;
+using Rsp.IrasService.Infrastructure;
+using Rsp.IrasService.Infrastructure.Repositories;
+using Rsp.IrasService.Services;
+
+namespace Rsp.IrasService.UnitTests.Services.RespondentServiceTests;
+
+/// <summary>
+///     Covers the tests for SaveModificationParticipatingOrganisationAnswers method
+/// </summary>
+public class SaveModificationParticipatingOrganisationAnswers : TestServiceBase<RespondentService>
+{
+    private readonly RespondentRepository _personnelRepository;
+    private readonly IrasContext _context;
+
+    public SaveModificationParticipatingOrganisationAnswers()
+    {
+        var options = new DbContextOptionsBuilder<IrasContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString("N")).Options;
+
+        _context = new IrasContext(options);
+        _personnelRepository = new RespondentRepository(_context);
+    }
+
+    /// <summary>
+    ///     Tests that modification documents are saved
+    /// </summary>
+    [Theory, AutoData]
+    public async Task Persists_ModificationParticipatingOrganisationAnswers(ModificationParticipatingOrganisationAnswerDto request)
+    {
+        // Arrange
+        Mocker.Use<IProjectPersonnelRepository>(_personnelRepository);
+
+        Sut = Mocker.CreateInstance<RespondentService>();
+
+        // Act
+        await Sut.SaveModificationParticipatingOrganisationAnswerResponses(request);
+
+        // Assert
+        var actualCount = await _context.ModificationParticipatingOrganisationAnswers.CountAsync();
+        actualCount.ShouldBe(1);
+    }
+}
