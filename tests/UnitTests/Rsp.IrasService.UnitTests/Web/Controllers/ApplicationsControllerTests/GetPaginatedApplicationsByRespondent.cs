38fda8ab9738dc5e61b17c6a3f9e9442ebca9289
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-07-22 12:40:45 +0200

    Rsp 3881 new research page (#40)
    * Added pagination possibility to GetApplicationsByRespondent
    
    * Separated Get paginated applications endpoint from Get all applications for the respondend
    
    Added total count for respond.
    
    * Added unit tests to cover GetPaginatedApplicationsWithRespondent endpoint
    
    * Implemented style improvements as described in PR comments

---
+++ tests/UnitTests/Rsp.IrasService.UnitTests/Web/Controllers/ApplicationsControllerTests/GetPaginatedApplicationsByRespondent.cs
@@ -0,0 +1,85 @@
+﻿using Rsp.IrasService.Application.CQRS.Queries;
+using Rsp.IrasService.Application.DTOS.Responses;
+using Rsp.IrasService.WebApi.Controllers;
+
+namespace Rsp.IrasService.UnitTests.Web.Controllers.ApplicationsControllerTests;
+
+public class GetPaginatedApplicationsByRespondent : TestServiceBase
+{
+    private readonly ApplicationsController _controller;
+
+    public GetPaginatedApplicationsByRespondent()
+    {
+        _controller = Mocker.CreateInstance<ApplicationsController>();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task GetPaginatedApplicationsByRespondent_ShouldReturnApplications_WhenDataExists(string respondentId,
+        PaginatedResponse<ApplicationResponse> mockResponse)
+    {
+        // Arrange
+        var mockMediator = Mocker.GetMock<IMediator>();
+        mockMediator
+            .Setup
+            (
+                m => m.Send
+                (
+                    It.Is<GetPaginatedApplicationsWithRespondentQuery>
+                    (
+                        q =>
+                            q.RespondentId == respondentId &&
+                            q.SearchQuery == null &&
+                            q.PageIndex == 1 &&
+                            q.PageSize == 5
+                    ),
+                    It.IsAny<CancellationToken>()
+                )
+            )
+            .ReturnsAsync(mockResponse);
+
+        // Act
+        var result = await _controller.GetPaginatedApplicationsByRespondent(respondentId, null, 1, 5);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.ShouldBeEquivalentTo(mockResponse);
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task GetPaginatedApplicationsByRespondent_ShouldReturnEmptyList_WhenNoDataExists(string respondentId)
+    {
+        // Arrange
+        var mockMediator = Mocker.GetMock<IMediator>();
+        mockMediator
+            .Setup
+            (
+                m => m.Send
+                (
+                    It.Is<GetPaginatedApplicationsWithRespondentQuery>
+                        (
+                            q =>
+                                q.RespondentId == respondentId &&
+                                q.SearchQuery == null &&
+                                q.PageIndex == 1 &&
+                                q.PageSize == 5
+                        ),
+                    It.IsAny<CancellationToken>()
+                )
+            )
+            .ReturnsAsync(new PaginatedResponse<ApplicationResponse>
+            {
+                Items = new List<ApplicationResponse>(),
+                TotalCount = 0
+            });
+
+        // Act
+        var result = await _controller.GetPaginatedApplicationsByRespondent(respondentId, null, 1, 5);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Items.ShouldBeEmpty();
+        result.TotalCount.ShouldBe(0);
+    }
+}
