3ae61957d352149db4bb100576dcba8ae9698abd
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-08-01 10:06:00 +0100

    - seeding document types

---
+++ src/Infrastructure/Rsp.IrasService.Infrastructure/Migrations/20250731185143_AddDocumentTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rsp.IrasService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DocumentTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("8d5c4d33-963b-4e52-a2ee-0f1cb97ba6d3"), "Protocol / Clinical Investigation Plan" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DocumentTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("8d5c4d33-963b-4e52-a2ee-0f1cb97ba6d3"));
+        }
+    }
+}
