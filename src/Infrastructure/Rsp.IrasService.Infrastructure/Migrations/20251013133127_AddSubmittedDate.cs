34f16eb0d257fe3968ae7afd5c9aaa95187cfbf0
ShilpaHugar <Shilpa.hugar@paconsulting.com>
2025-10-13 14:32:21 +0100

    migration script

---
+++ src/Infrastructure/Rsp.IrasService.Infrastructure/Migrations/20251013133127_AddSubmittedDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rsp.IrasService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubmittedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
