3b8b0b46c95ab78828cf855499d90146a19c80a3
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-11-13 16:10:07 +0100

    Adjusted modification status text (#117)

--- src/Application/Rsp.IrasService.Application/Constants/ModificationStatus.cs
+++ src/Application/Rsp.IrasService.Application/Constants/ModificationStatus.cs
@@ -3,13 +3,8 @@
 public struct ModificationStatus
 {
     public const string InDraft = "In draft";
-    public const string ChangeReadyForSubmission = "Change ready for submission";
-    public const string Unfinished = "Unfinished";
     public const string WithSponsor = "With sponsor";
-    public const string WithRegulator = "With regulator";
     public const string WithReviewBody = "With review body";
     public const string Approved = "Approved";
     public const string NotApproved = "Not approved";
-    public const string Authorised = "Authorised";
-    public const string NotAuthorised = "Not authorised";
 }
