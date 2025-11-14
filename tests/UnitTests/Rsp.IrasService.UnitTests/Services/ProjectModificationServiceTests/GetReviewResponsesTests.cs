12646e7ee6766b51bba7e76c56b6866ce351902b
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-11-14 16:12:48 +0000

    RSP-4825,4827,4828,4829,4831: SWR review modification journey

---
+++ tests/UnitTests/Rsp.IrasService.UnitTests/Services/ProjectModificationServiceTests/GetReviewResponsesTests.cs
@@ -0,0 +1,31 @@
+﻿using Rsp.IrasService.Application.Contracts.Repositories;
+using Rsp.IrasService.Domain.Entities;
+using Rsp.IrasService.Services;
+
+namespace Rsp.IrasService.UnitTests.Services.ProjectModificationServiceTests;
+
+public class GetReviewResponsesTests : TestServiceBase<ProjectModificationService>
+{
+    [Theory, AutoData]
+    public async Task Returns_ReviewResponses_For_Given_ModificationId
+    (
+        Guid projectModificationId,
+        ProjectModification repoResponse
+    )
+    {
+        // Arrange
+        var repo = Mocker.GetMock<IProjectModificationRepository>();
+        repo.Setup(r => r.GetModificationById(It.IsAny<Guid>())).ReturnsAsync(repoResponse);
+
+        // Act
+        Sut = Mocker.CreateInstance<ProjectModificationService>();
+        var result = await Sut.GetModificationReviewResponses(projectModificationId);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.ModificationId.ShouldBe(repoResponse.Id);
+        result.Comment.ShouldBe(repoResponse.ReviewerComments);
+        result.ReasonNotApproved.ShouldBe(repoResponse.ReasonNotApproved);
+        result.ReviewOutcome.ShouldBe(repoResponse.ProvisionalReviewOutcome);
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasService.UnitTests/Web/Controllers/ProjectModificationsControllerTests/GetReviewResponses.cs
@@ -0,0 +1,26 @@
+﻿using Rsp.IrasService.Application.CQRS.Queries;
+using Rsp.IrasService.Application.DTOS.Responses;
+using Rsp.IrasService.WebApi.Controllers;
+
+namespace Rsp.IrasService.UnitTests.Web.Controllers.ProjectModificationsControllerTests;
+
+public class GetReviewResponses : TestServiceBase<ProjectModificationsController>
+{
+    [Theory, AutoData]
+    public async Task GetReviewResponses_ShouldReturnModificationReviewResponse_WhenValidRequest
+    (
+        Guid modificationId,
+        ModificationReviewResponse expectedResponse
+    )
+    {
+        // Arrange
+        var mockMediator = Mocker.GetMock<IMediator>();
+        mockMediator.Setup(m => m.Send(It.IsAny<GetModificationReviewResponsesQuery>(), default))
+            .ReturnsAsync(expectedResponse);
+
+        // Act
+        var result = await Sut.GetReviewResponses(modificationId);
+
+        result.ShouldBeEquivalentTo(expectedResponse);
+    }
+}
