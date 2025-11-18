39da9c6fc674a8dd03b6335c8190f9d9ad6d6bc5
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-11-18 14:23:16 +0000

    Updated with RSP-6232

--- src/Infrastructure/Rsp.IrasService.Infrastructure/Repositories/ProjectModificationRepository.cs
+++ src/Infrastructure/Rsp.IrasService.Infrastructure/Repositories/ProjectModificationRepository.cs
@@ -280,7 +280,7 @@ public class ProjectModificationRepository(IrasContext irasContext) : IProjectMo
                 (string.IsNullOrEmpty(searchQuery.IrasId)
                  || x.IrasId.Contains(searchQuery.IrasId, StringComparison.OrdinalIgnoreCase))
                 && (string.IsNullOrEmpty(searchQuery.ModificationId)
-                 || x.ModificationId.Contains(searchQuery.ModificationId, StringComparison.OrdinalIgnoreCase))
+                    || x.ModificationId.Contains(searchQuery.ModificationId, StringComparison.OrdinalIgnoreCase))
                 && (string.IsNullOrEmpty(searchQuery.ChiefInvestigatorName)
                     || x.ChiefInvestigator.Contains(searchQuery.ChiefInvestigatorName,
                         StringComparison.OrdinalIgnoreCase))
@@ -311,7 +311,9 @@ public class ProjectModificationRepository(IrasContext irasContext) : IProjectMo
                 && (string.IsNullOrEmpty(searchQuery.ModificationType) ||
                     x.ModificationType.Contains(searchQuery.ModificationType, StringComparison.OrdinalIgnoreCase))
                 && (string.IsNullOrEmpty(searchQuery.Status) ||
-                    x.Status.Contains(searchQuery.Status, StringComparison.OrdinalIgnoreCase)));
+                    x.Status.Contains(searchQuery.Status, StringComparison.OrdinalIgnoreCase))
+                && (!searchQuery.AllowedStatuses.Any()
+                    || searchQuery.AllowedStatuses.Contains(x.Status, StringComparer.OrdinalIgnoreCase)));
     }
 
     private static IEnumerable<ProjectModificationResult> SortModifications(
