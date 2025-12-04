938fad8e48977fdc3f1ad940ba0f8a5ff729cbc4
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-12-04 12:21:36 +0000

    - avoid duplicate code

--- src/Domain/Rsp.IrasService.Domain/Entities/ProjectModificationResult.cs
+++ src/Domain/Rsp.IrasService.Domain/Entities/ProjectModificationResult.cs
@@ -1,26 +1,8 @@
 ﻿namespace Rsp.IrasService.Domain.Entities;
 
-public class ProjectModificationResult
+public class ProjectModificationResult : ModificationBase
 {
-    public string Id { get; set; } = null!;
-    public string ProjectRecordId { get; set; } = null!;
-    public string ModificationId { get; set; } = null!;
     public string IrasId { get; set; } = null!;
     public int ModificationNumber { get; set; }
-    public string ShortProjectTitle { get; set; } = null!;
-    public string ModificationType { get; set; } = null!;
-    public string Category { get; set; } = null!;
-    public string ReviewType { get; set; } = null!;
-    public string ChiefInvestigatorFirstName { get; set; } = null!;
-    public string ChiefInvestigatorLastName { get; set; } = null!;
-    public string ChiefInvestigator { get; set; } = null!;
-    public string LeadNation { get; set; } = null!;
-    public string ParticipatingNation { get; set; } = null!;
-    public string SponsorOrganisation { get; set; } = null!;
-    public DateTime CreatedAt { get; set; }
-    public string? ReviewerId { get; set; } = null;
-    public string? ReviewerName { get; set; } = null;
-    public string Status { get; set; } = null!;
-    public DateTime? SentToRegulatorDate { get; set; }
-    public DateTime? SentToSponsorDate { get; set; }
+    public string? ReviewerId { get; set; }
 }
