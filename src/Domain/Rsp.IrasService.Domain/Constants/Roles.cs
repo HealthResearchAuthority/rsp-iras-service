c38cc426bad5d2dfa9fcffd0c9ac22372acd5bb5
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-12-16 16:28:18 +0000

    RSP-6424 Add Organisation Administrator Role (#149)

--- src/Domain/Rsp.IrasService.Domain/Constants/Roles.cs
+++ src/Domain/Rsp.IrasService.Domain/Constants/Roles.cs
@@ -8,4 +8,5 @@ public static class Roles
     public const string WorkflowCoordinator = "workflow_co-ordinator";
     public const string StudyWideReviewer = "study-wide_reviewer";
     public const string SystemAdministrator = "system_administrator";
+    public const string OrganisationAdministrator = "organisation_administrator";
 }
