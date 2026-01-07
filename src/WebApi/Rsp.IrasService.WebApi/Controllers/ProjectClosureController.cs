5f992a8927c93e746073a5b38993c8c443995e40
ShilpaHugar <Shilpa.hugar@paconsulting.com>
2026-01-07 11:22:15 +0000

    RSP-4908

--- src/WebApi/Rsp.IrasService.WebApi/Controllers/ProjectClosureController.cs
+++ src/WebApi/Rsp.IrasService.WebApi/Controllers/ProjectClosureController.cs
@@ -33,8 +33,8 @@ public class ProjectClosureController(IMediator mediator) : ControllerBase
     /// Returns a single project closure record
     /// </summary>
     /// <param name="projectRecordId">Researcher Project Record Id</param>
-    [HttpGet("getprojectclosure")]
-    public async Task<ActionResult<ProjectClosureResponse>> GetProjectClosure(string projectRecordId)
+    [HttpGet("getprojectclosurebyid")]
+    public async Task<ActionResult<ProjectClosureResponse>> GetProjectClosureById(string projectRecordId)
     {
         var request = new GetProjectClosureQuery(projectRecordId);
 
