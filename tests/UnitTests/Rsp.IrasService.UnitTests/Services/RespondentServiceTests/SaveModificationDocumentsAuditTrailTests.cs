515574baae28f9cf7e6330accb697becfdbcae99
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2026-01-12 14:39:02 +0000

    - creating new unit tests for the methods

---
+++ tests/UnitTests/Rsp.IrasService.UnitTests/Services/RespondentServiceTests/SaveModificationDocumentsAuditTrailTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rsp.IrasService.Application.Contracts.Repositories;
+using Rsp.IrasService.Application.DTOS.Requests;
+using Rsp.IrasService.Infrastructure;
+using Rsp.IrasService.Infrastructure.Repositories;
+using Rsp.IrasService.Services;
+
+namespace Rsp.IrasService.UnitTests.Services.RespondentServiceTests;
+
+public class SaveModificationDocumentsAuditTrailTests : TestServiceBase<RespondentService>
+{
+    private readonly RespondentRepository _personnelRepository;
+    private readonly IrasContext _context;
+
+    public SaveModificationDocumentsAuditTrailTests()
+    {
+        var options = new DbContextOptionsBuilder<IrasContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString("N")).Options;
+
+        _context = new IrasContext(options);
+        _personnelRepository = new RespondentRepository(_context);
+    }
+
+    [Theory, AutoData]
+    public async Task SaveModificationDocumentsAuditTrail_PersistsAuditEntries(
+    List<ModificationDocumentsAuditTrailDto> auditDtos)
+    {
+        // Arrange
+        Mocker.Use<IProjectPersonnelRepository>(_personnelRepository);
+        Sut = Mocker.CreateInstance<RespondentService>();
+
+        // Act
+        await Sut.SaveModificationDocumentsAuditTrail(auditDtos);
+
+        // Assert
+        var savedEntries = await _context.ModificationDocumentsAuditTrail.ToListAsync();
+
+        savedEntries.Count.ShouldBe(auditDtos.Count);
+
+        // Optional: spot-check mapping correctness
+        for (int i = 0; i < auditDtos.Count; i++)
+        {
+            savedEntries[i].Description.ShouldBe(auditDtos[i].Description);
+            savedEntries[i].FileName.ShouldBe(auditDtos[i].FileName);
+            savedEntries[i].User.ShouldBe(auditDtos[i].User);
+            savedEntries[i].ProjectModificationId.ShouldBe(auditDtos[i].ProjectModificationId);
+        }
+    }
+
+    [Fact]
+    public async Task SaveModificationDocumentsAuditTrail_WithEmptyList_DoesNothing()
+    {
+        // Arrange
+        Mocker.Use<IProjectPersonnelRepository>(_personnelRepository);
+        Sut = Mocker.CreateInstance<RespondentService>();
+
+        // Act
+        await Sut.SaveModificationDocumentsAuditTrail(new List<ModificationDocumentsAuditTrailDto>());
+
+        // Assert
+        var count = await _context.ModificationDocumentsAuditTrail.CountAsync();
+        count.ShouldBe(0);
+    }
+
+    [Fact]
+    public async Task SaveModificationDocumentsAuditTrail_WithNullList_DoesNothing()
+    {
+        // Arrange
+        Mocker.Use<IProjectPersonnelRepository>(_personnelRepository);
+        Sut = Mocker.CreateInstance<RespondentService>();
+
+        // Act
+        await Sut.SaveModificationDocumentsAuditTrail(null!);
+
+        // Assert
+        var count = await _context.ModificationDocumentsAuditTrail.CountAsync();
+        count.ShouldBe(0);
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasService.UnitTests/Web/Controllers/DocumentsControllerTests/CreateModificationDocumentsAuditTrail.cs
@@ -0,0 +1,55 @@
+﻿using Rsp.IrasService.Application.CQRS.Commands;
+using Rsp.IrasService.Application.DTOS.Requests;
+using Rsp.IrasService.WebApi.Controllers;
+
+namespace Rsp.IrasService.UnitTests.Web.Controllers.DocumentsControllerTests;
+
+public class CreateModificationDocumentsAuditTrail : TestServiceBase<DocumentsController>
+{
+    [Theory]
+    [AutoData]
+    public async Task CreateModificationDocumentsAuditTrail_SendsCommand_WithExactDtoList(
+    List<ModificationDocumentsAuditTrailDto> auditDtos)
+    {
+        // Arrange
+        var mockMediator = Mocker.GetMock<IMediator>();
+
+        mockMediator
+            .Setup(m => m.Send(
+                It.IsAny<SaveModificationDocumentsAuditTrailCommand>(),
+                CancellationToken.None))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        await Sut.CreateModificationDocumentsAuditTrail(auditDtos);
+
+        // Assert
+        mockMediator.Verify(m => m.Send(
+            It.Is<SaveModificationDocumentsAuditTrailCommand>(
+                c => c.DocumentsAuditTrailRequest == auditDtos),
+            CancellationToken.None),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateModificationDocumentsAuditTrail_WithEmptyList_StillSendsCommand()
+    {
+        // Arrange
+        var auditDtos = new List<ModificationDocumentsAuditTrailDto>();
+
+        var mockMediator = Mocker.GetMock<IMediator>();
+        mockMediator
+            .Setup(m => m.Send(It.IsAny<SaveModificationDocumentsAuditTrailCommand>(), CancellationToken.None))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        await Sut.CreateModificationDocumentsAuditTrail(auditDtos);
+
+        // Assert
+        mockMediator.Verify(m => m.Send(
+            It.Is<SaveModificationDocumentsAuditTrailCommand>(
+                c => c.DocumentsAuditTrailRequest.Count == 0),
+            CancellationToken.None),
+            Times.Once);
+    }
+}
